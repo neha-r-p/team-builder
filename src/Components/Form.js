0c7a01953b25a395561154c19ea3dced3b3ee2e9
Neha Patel <neha.r.p@gmail.com>
2019-07-24 16:38:19 -0700

    Form.js added a submit button

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,39 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Form() {
-    const [teamMember, setTeamMember] = useState({name:'', email:'', role:''})
+  const [teamMember, setTeamMember] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
 
-    function handleChange(event){
-        const updatedTeamMember = { ...teamMember, [event.target.name]: event.target.value }
+  function handleChange(event) {
+    const updatedTeamMember = {
+      ...teamMember,
+      [event.target.name]: event.target.value
+    };
+    console.log(
+        "handleChange",
+        event.target.name,
+        event.target.value,
+        updatedTeamMember
+      );
 
-        setTeamMember(updatedTeamMember);
-    }
+    setTeamMember(updatedTeamMember);
+  }
 
-    function handleSubmit(event){
-        event.preventDefault();
-        console.log("team member state", teamMember)
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("team member state", teamMember);
+  }
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <fieldset>
-                <legend>Team Member Sign-up</legend>
-                <div>
-                    <label for="name">Name
-                    <div>
-                        <input 
-                            type="text"
-                            name="name"
-                            placeholder="Your name here"
-                            value={teamMember.name}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    </label>
-                </div>
-            </fieldset>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Team Member Sign-up</legend>
+        <div>
+          <label for="name">
+            Name
+            <div>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name here"
+                value={teamMember.name}
+                onChange={handleChange}
+              />
+            </div>
+          </label>
+        </div>
+        <button type="submit">
+          Submit
+        </button>
+      </fieldset>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
