3881d6518defe1d4597a65ab3dc6150cba97c6ba
Neha Patel <neha.r.p@gmail.com>
2019-07-24 13:25:20 -0700

    Form.js added change handler

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react';
 
 function Form() {
     const [teamMember, setTeamMember] = useState({name:'', email:'', role:''})
+
+    function handleChange(event){
+        const updatedTeamMember = { ...teamMember, [event.target.name]: event.target.value }
+        
+        setTeamMember(updatedTeamMember);
+    }
+
     return(
         <div>
             <p>HI</p>
